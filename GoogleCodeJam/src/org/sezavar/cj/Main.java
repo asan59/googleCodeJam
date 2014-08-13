ae74934fb0c72c29202123410bc2cac0113bec9f
amir <amir@Amir-Win-Laptop>
2014-08-13 19:20:06 +0430

    adding new problems

--- GoogleCodeJam/src/org/sezavar/cj/Main.java
+++ GoogleCodeJam/src/org/sezavar/cj/Main.java
@@ -1,5 +1,20 @@
 package org.sezavar.cj;
 
-public class Main {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.util.Scanner;
 
+import org.sezavar.cj.util.GoogleCodeJamUtil;
+
+public class Main {
+	public static void main(String[] args) {
+		InputStream input = GoogleCodeJamUtil.class
+				.getResourceAsStream("/StoreCredit/"
+						+ GoogleCodeJamUtil.SMALL_INPUT_FILE_NAME);
+		Scanner reader = new Scanner(input);
+		System.out.println(reader.nextLine());
+		reader.close();
+	}
 }
---
+++ GoogleCodeJam/src/org/sezavar/cj/util/GoogleCodeJamUtil.java
@@ -0,0 +1,12 @@
+package org.sezavar.cj.util;
+
+import java.util.Scanner;
+
+public class GoogleCodeJamUtil {
+	public final static String SMALL_INPUT_FILE_NAME = "small-practice.in";
+	public final static String LARGE_INPUT_FILE_NAME = "large-practice.in";
+	
+//	public final static Scanner getReaderFor(){
+		
+//	}
+}
