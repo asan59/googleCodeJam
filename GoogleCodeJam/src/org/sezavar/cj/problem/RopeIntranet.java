07927e1f1423a49d25c828b3c468d32a495a183f
asan59 <amir@192.168.1.214>
2014-08-14 02:25:54 +0430

    a new problem is added

---
+++ GoogleCodeJam/src/org/sezavar/cj/problem/RopeIntranet.java
@@ -0,0 +1,25 @@
+package org.sezavar.cj.problem;
+
+import org.sezavar.cj.util.InputOutputType;
+
+public class RopeIntranet extends Problem {
+
+	@Override
+	protected StringBuffer _solve() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected StringBuffer _solveEachSet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static void main(String[] args) {
+		Problem pr = new RopeIntranet();
+		pr.solveSetsFor(InputOutputType.VERY_SMALL);
+
+	}
+
+}
